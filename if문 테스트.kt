25e58f7484e378effe485d6bc87714594d7dad66
jjoMooNo <jjomoono@gmail.com>
2025-05-29 00:34:00 +0900

    djdj

---
+++ if문 테스트.kt
@@ -0,0 +1,11 @@
+//if문 테스트
+fun main() {
+    var venatoria: Int = 424
+    var takanashi: Int = 102
+    
+    if(venatoria == takanashi) {
+        println("Is Venatoria Hoshino?")
+    } else if(venatoria > takanashi) {
+        println("Yeah it is true :D")
+    }
+}
