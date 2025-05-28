fae84047afa6265532a774323ec9ce2b72790bdf
jjoMooNo <jjomoono@gmail.com>
2025-05-28 23:58:15 +0900

    Create Variable_Test.kt
    변수

---
+++ Variable_Test.kt
@@ -0,0 +1,20 @@
+//변수 테스트
+fun main() {
+    var venatoria: Any? = null
+    var takanashi: Int = 102
+    var misono: Long = 508L
+    var rikuhachima: Double = 3.12
+    var tendou: Float = 3.25F
+    var tachibana: Char = 'N'
+    var sunaokami: String = "Shiroko"
+    var kivotos: Boolean = true
+    var atrahasis: Boolean = false
+    
+    println(takanashi)
+    println(misono)
+    println(rikuhachima)
+    println(tendou)
+    println(tachibana)
+    println(sunaokami)
+    println(venatoria)
+}
