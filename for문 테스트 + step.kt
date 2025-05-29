23c313a039176e3c4fd5d3c1e647f44576859c73
jjoMooNo <jjomoono@gmail.com>
2025-05-29 22:48:04 +0900

    Update for문 테스트 + step.kt

--- for문 테스트 + step.kt
+++ for문 테스트 + step.kt
@@ -4,10 +4,21 @@ fun main() {
         venatoria = i
         print(venatoria)
     }
-    
     println()
     
     for(i in 0..9 step 2) {
+        venatoria = i
+        print(venatoria)
+    }
+    println()
+    
+    for(i in 9 downTo 0) {
+        venatoria = i
+        print(venatoria)
+    }
+    println()
+    
+    for(i in 9 downTo 0 step 2) {
         venatoria = i
         print(venatoria)
     }
