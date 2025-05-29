a4de2d8383f692513032456edf7a9125eadc16a3
jjoMooNo <jjomoono@gmail.com>
2025-05-29 22:43:02 +0900

    Create for문 테스트 + step.kt

---
+++ for문 테스트 + step.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var venatoria: Int = 0
+    for(i in 0..9) {
+        venatoria = i
+        print(venatoria)
+    }
+    
+    println()
+    
+    for(i in 0..9 step 2) {
+        venatoria = i
+        print(venatoria)
+    }
+}
